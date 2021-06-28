5daff45758aac343c424687ed6bec8fb26ce4062
valentint <valentin.todorov@chello.at>
2021-06-28 15:08:36 +0200

    Update Bug-examples-2.R

--- inst/work/Bug-examples-2.R
+++ inst/work/Bug-examples-2.R
@@ -2,32 +2,15 @@
 ##  VT:: Bug examples in fsdaR as of 28.06.2021
 ##
 
-## 1. Test case 1: Issue with to slow plotting in FSM()
+## 1. Test case 1: General issue with TCLUST
 ##
-
-library(fsdaR)
-
-n <- 200
-v <- 3
-set.seed(123456)
-X <- matrix(rnorm(n*v), nrow=n)
-Xcont <- X
-Xcont[1:5, ] <- Xcont[1:5,] + 3
-
-(out <- fsmult(Xcont))               # no plots, works fine
-
-## With plots, it will take v-e-e-e-ry long to complete.
-##  It was not like this in an older version of FSDA.
-out <- fsmult(Xcont, plot=TRUE)
-
-
-## 2. Test case 2: General issue with TCLUST
-##
+##  Reported by Domenico.
 ##  According to Domenico, this happened after they wrote a function in C, see the file DfM.C
-
+##  Please check with Domenico about the details of the C function
+##
 ## The following error will be thrown:
 ##  Error in .jcall(fsdaEngine, returnType, fsdaFunction, as.integer(nargout),  :
-##    com.mathworks.toolbox.javabuilder.MWException: Invalid MEX-file 'C:\Users\valen\AppData\Local\Temp\valen\mcrCache9.6\fsda4j0\fsda4java\FSDA-src\utilities_stat\DfM.mexw64': 
+##    com.mathworks.toolbox.javabuilder.MWException: Invalid MEX-file 'C:\Users\valen\AppData\Local\Temp\valen\mcrCache9.6\fsda4j0\fsda4java\FSDA-src\utilities_stat\DfM.mexw64':
 ##      Missing dependent shared libraries:
 ##      'icudt61.dll' required by 'C:\Program Files\MATLAB\MATLAB Runtime\v96/bin/win64\icuuc61.dll->C:\Program Files\MATLAB\MATLAB Runtime\v96/bin/win64\icuin61.dll->C:\Program
 ##      Files\MATLAB\MATLAB Runtime\v96/bin/win64\libmwi18n.dll->C:\Program Files\MATLAB\MATLAB
@@ -50,7 +33,7 @@ x[, ncol(x)] <- rnorm(nrow(Xmix))
 (out <- tclustfsda(Xmix, k=3, alpha=0))
 
 
-## 3. Test case 3: An error in fsmult() with a specific data set: 'geyser' from MASS
+## 2. Test case 2: An error in fsmult() with a specific data set: 'geyser' from MASS
 library(fsdaR)
 library(MASS)
 data(geyser2)
@@ -65,3 +48,22 @@ out <- fsmult(geyser)                           # 2. Will throw an exception
 
 out <- fsmult(matrix(rnorm(299*2), ncol=2))     # 3. Works OK
 out <- fsmult(geyser2)                          # 4. Works OK
+
+## 3. Test case 3: Issue with too slow plotting in FSM()
+##      Reported by Andrea Cappuzzo. He says that R frezes (on Mac).
+##      Actually, my observation (on Windows) is that it takes too long to show the plot.
+
+library(fsdaR)
+
+n <- 200
+v <- 3
+set.seed(123456)
+X <- matrix(rnorm(n*v), nrow=n)
+Xcont <- X
+Xcont[1:5, ] <- Xcont[1:5,] + 3
+
+(out <- fsmult(Xcont))               # no plots, works fine
+
+## With plots, it will take v-e-e-e-ry long to complete.
+##  It was not like this in an older version of FSDA.
+out <- fsmult(Xcont, plot=TRUE)
